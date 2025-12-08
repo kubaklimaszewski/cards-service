ece6390af94810a75efe4eac7603776a112aa851
Jakub Klimaszewski <kuba.adam.klimaszewski.@gmail.com>
2025-12-08 20:43:53 +0100

    Losowanie card, dodawanie do użytkownika

--- server/src/db/data.sql
+++ server/src/db/data.sql
@@ -1,4 +1,12 @@
 INSERT INTO packs (name, price, description, rarity, icon, cards_count) VALUES 
     ('Basic Pack', 100, 'Opisowy opis', 'common', '&#x1F4E6;', 5),
-    ('50/50 Pack', 2000, 'Win or shit', 'mythic', '&#x1F409;', 1);
+    ('50/50 Pack', 2000, 'Win or shit', 'mythic', '&#x1F409;', 1),
     ('Bird Pack', 250, 'Znbieraj karty ptaków', 'rare', '&#x1F424;', 3);
+
+INSERT INTO cards (name, value, description, rarity, icon) VALUES
+    ('Shit', 1, 'Jebać opisy', 'common', '&#x1f4a9;'),
+    ('Dragon', 3200, 'Jebać opisy v2', 'mythic', '&#x1f409;');
+
+INSERT INTO packs_cards (pack_id, card_id, drop_weight) VALUES 
+    (2, 1, 1),
+    (2, 2, 1);
--- server/src/db/migrations.sql
+++ server/src/db/migrations.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS users (
   username VARCHAR(32) NOT NULL,
   email VARCHAR(100) UNIQUE NOT NULL,
   password_hash VARCHAR(255) NOT NULL,
-  balance INT DEFAULT 500,
+  balance INTEGER DEFAULT 500,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
@@ -29,5 +29,42 @@ CREATE TABLE IF NOT EXISTS users_packs (
     ON DELETE CASCADE,
   CONSTRAINT fk_users_packs_pack
     FOREIGN KEY (pack_id) REFERENCES packs(id)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS cards (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+  value INTEGER NOT NULL CHECK (value > 0),
+  description TEXT,
+  rarity VARCHAR(30) NOT NULL,
+  icon VARCHAR(10) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  UNIQUE(name) 
+);
+
+CREATE TABLE packs_cards (
+  pack_id INTEGER NOT NULL,
+  card_id INTEGER NOT NULL,
+  drop_weight INTEGER NOT NULL CHECK (drop_weight > 0), 
+  PRIMARY KEY (pack_id, card_id),
+  CONSTRAINT fk_packs_cards_pack
+    FOREIGN KEY (pack_id) REFERENCES packs(id)
+    ON DELETE CASCADE,
+  CONSTRAINT fk_packs_cards_card
+    FOREIGN KEY (card_id) REFERENCES cards(id)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS users_cards (
+  user_id INTEGER NOT NULL,
+  card_id INTEGER NOT NULL,
+  quantity INTEGER NOT NULL DEFAULT 0 CHECK (quantity >= 0),
+  PRIMARY KEY (user_id, card_id),
+  CONSTRAINT fk_users_cards_user
+    FOREIGN KEY (user_id) REFERENCES users(id)
+    ON DELETE CASCADE,
+  CONSTRAINT fk_users_cards_card
+    FOREIGN KEY (card_id) REFERENCES cards(id)
     ON DELETE CASCADE
 );
